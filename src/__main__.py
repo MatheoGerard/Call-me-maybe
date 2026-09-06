32304abec056c7ec97759b524cdb03b6beb69dcb
Matheo Gerard <mgerard@fu-r4-p3.s19.be>
2026-09-06 19:40:24 +0200

    initial commit with uv structure

---
+++ src/__main__.py
@@ -0,0 +1,35 @@
+from llm_sdk import Small_LLM_Model
+import numpy as np
+
+
+def main() -> None:
+    print("LOAD LLM...")
+    llm: Small_LLM_Model = Small_LLM_Model()
+
+    prompt: str = "Q: What is the sum of 2 and 3?\nA:"
+    context_tokens: list[int] = llm.encode(prompt)[0].tolist()
+
+    # ID explicite extrait du vocabulaire
+    EOS_TOKEN_ID: int = 84399
+
+    print(f"Prompt:\n{prompt}")
+    print("Réponse: ", end="", flush=True)
+
+    for _ in range(50):
+        probabilities = llm.get_logits_from_input_ids(context_tokens)
+        next_token: int = int(np.argmax(probabilities))
+
+        # Arrêt immédiat si le token EOS est prédit
+        if next_token == EOS_TOKEN_ID:
+            break
+
+        context_tokens.append(next_token)
+
+        word: str = llm.decode([next_token])
+        print(word, end="", flush=True)
+
+    print("\n")
+
+
+if __name__ == "__main__":
+    main()
