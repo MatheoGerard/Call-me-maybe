c5c6ad9340549e5d4d70abcc9b44f7ca9f5d39cd
Matheo Gerard <mgerard@fu-r4-p3.s19.be>
2026-09-06 19:44:38 +0200

    change tu keep only test to learn llm logic

--- src/__main__.py
+++ src/__main__.py
@@ -9,19 +9,12 @@ def main() -> None:
     prompt: str = "Q: What is the sum of 2 and 3?\nA:"
     context_tokens: list[int] = llm.encode(prompt)[0].tolist()
 
-    # ID explicite extrait du vocabulaire
-    EOS_TOKEN_ID: int = 84399
-
     print(f"Prompt:\n{prompt}")
     print("Réponse: ", end="", flush=True)
 
     for _ in range(50):
         probabilities = llm.get_logits_from_input_ids(context_tokens)
         next_token: int = int(np.argmax(probabilities))
-
-        # Arrêt immédiat si le token EOS est prédit
-        if next_token == EOS_TOKEN_ID:
-            break
 
         context_tokens.append(next_token)
 
